48150e1c0d3e3291e78a3824a26c56541ecfba74
Julian <julian.57@outlook.fr>
2023-04-04 14:24:00 +0200

    make a test

--- GoldenLineApp/urls.py
+++ GoldenLineApp/urls.py
@@ -5,7 +5,7 @@ from DataAnalyse.views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    #path('', include('django.contrib.auth.urls'), name='login'),
+    path('', include('django.contrib.auth.urls'), name='login'),
     #path('', auth_views.LoginView.as_view(), name='login'),
     #path('data-analyse', index, name='analyse'),
     
