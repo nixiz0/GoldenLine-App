c2810652a0a537cec17bd20e7064d98c2423e656
Julian <julian.57@outlook.fr>
2023-04-04 14:55:50 +0200

    ctrl-z

--- GoldenLineApp/urls.py
+++ GoldenLineApp/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
-from .views import analyseData
+from DataAnalyse.views import analyseData
 
 urlpatterns = [
     path('admin/', admin.site.urls),
--- GoldenLineApp/views.py
+++ GoldenLineApp/views.py
@@ -1,10 +1,4 @@
 from django.shortcuts import render
-from django.contrib.auth.decorators import login_required
-
 
 def index(request):
     return render(request, 'registration/login.html')
-
-@login_required
-def analyseData(request):
-    return render(request, 'analyse/analyse_index.html')
