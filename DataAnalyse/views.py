57777662c2c23353588406bd4490fceed9623af8
Julian <julian.57@outlook.fr>
2023-04-04 14:32:20 +0200

    Change the function name

--- DataAnalyse/views.py
+++ DataAnalyse/views.py
@@ -1,6 +1,6 @@
-#from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
-#@login_required
-def index(request):
-    return render(request, 'analyse/index.html')
+@login_required
+def analyseData(request):
+    return render(request, 'analyse/analyse_index.html')
