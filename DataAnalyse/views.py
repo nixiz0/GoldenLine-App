216df0ac1f4e23ac448fbfca88843d25d1f1432f
Julian <julian.57@outlook.fr>
2023-04-04 14:08:57 +0200

    Test del login

--- DataAnalyse/views.py
+++ DataAnalyse/views.py
@@ -1,6 +1,6 @@
-from django.contrib.auth.decorators import login_required
+#from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
-@login_required
+#@login_required
 def index(request):
     return render(request, 'analyse/index.html')
